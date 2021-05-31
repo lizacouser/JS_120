80418aea1cd986b0a804859858d031c122c7049f
Liza Couser <liza.couser@gmail.com>
2021-05-31 19:10:41 -0400

    Classes small problems

---
+++ small_problems/OOBasics_Classes/name_constructor.js
@@ -0,0 +1,10 @@
+/*
+Name the Constructor
+Update the following code so that, instead of logging the values,
+each statement logs the name of the constructor to which it belongs.
+*/
+
+console.log("Hello".constructor.name);   // string
+console.log([1,2,3].constructor.name);   // array
+console.log({name: 'Srdjan'}.constructor.name);  //object
+
